9aa9bd88ac2378861b77e2bd2325053f2b5fcabe
RaLLy08 <uytemataaero@mail.ru>
2021-12-19 17:37:26 +0400

    todo txt

---
+++ src/middlewares/types.ts
@@ -0,0 +1,13 @@
+// const mockMiddleware = jest.fn(
+//     <T>(arg: T): ((next: (arg: T) => void) => void) => {
+//       return (next) => {
+//         next(arg);
+//       };
+//     }
+// );
+    
+// const mockMiddleware = <T>(arg: T): (next: (arg: T) => void) => void => {
+//         return (next) => {
+//         next(arg);
+//     }
+// }
