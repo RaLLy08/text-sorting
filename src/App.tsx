ad309933fb3d3ac916f104c250bb8894eef3d571
RaLLy08 <uytemataaero@mail.ru>
2021-12-19 15:07:21 +0400

    test setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import AnimateText from './components/AnimateText'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div className='field'></div>
+    <div className="wrapper">
+      <div className='field'>
+        <AnimateText frames={[]}/>
+
+      </div>
     </div>
   )
 }
