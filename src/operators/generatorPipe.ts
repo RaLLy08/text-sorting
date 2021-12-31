02b33e1bf7528394d8d9664b422d1e52331dfd54
RaLLy08 <uytemataaero@mail.ru>
2022-01-01 02:51:16 +0400

    ts

--- src/operators/generatorPipe.ts
+++ src/operators/generatorPipe.ts
@@ -1,4 +1,15 @@
-const generatorPipe = (...generators) => function* (initialValue) {
+import { IndexedValueType } from './../types';
+
+interface GeneratorSuspended<T = IndexedValueType[], TReturn = IndexedValueType[], TNext = IndexedValueType[]> extends Iterator<T, TReturn, TNext> {
+    next(...args: [] | [TNext]): IteratorResult<T, TReturn>;
+    return(value: TReturn): IteratorResult<T, TReturn>;
+    throw(e: any): IteratorResult<T, TReturn>;
+    [Symbol.iterator](): GeneratorSuspended<T, TReturn, TNext>;
+}
+
+type Generator = (initialValue: IndexedValueType[]) => GeneratorSuspended
+
+const generatorPipe = (...generators: Generator[]) => function* (initialValue: IndexedValueType[]) {
     yield initialValue;
     let currentInitGenValue = initialValue;
 
